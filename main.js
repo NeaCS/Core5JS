dd6cad78514ec7d92ce8ddf8d28b2f4f3e76ec75
NeaCS <naldi.castro@gmail.com>
2026-05-15 22:30:35 -0400

    otras fuciones js

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 let botonAgregar = document.getElementById("boton-agregar")
-let botonMeGusta = document.getElementById("boton-like")
+let botonMeGustaCat = document.getElementById("boton-like-cat")
+let botonLogin = document.getElementById("boton-login")
+let botonesLike = document.getElementsByClassName("boton-like")
 
-botonAgregar.addEventListener("click", function() {
+botonAgregar.addEventListener("click", function () {
   this.remove()
 })
 
-botonMeGusta.addEventListener("click", function() {
+botonMeGustaCat.addEventListener("click", function () {
   alert("Gato Atigrado was liked")
 })
+
+botonLogin.addEventListener("click", function () {
+  this.innerHTML = "Cerrar sesión"
+})
+
+for (let i = 0; i < botonesLike.length; i++) {
+  botonesLike[i].addEventListener("click", function () {
+    let numeroLikes = parseInt(this.innerHTML)
+
+    numeroLikes++
+
+    this.innerHTML = numeroLikes + " me gusta"
+  })
+}
